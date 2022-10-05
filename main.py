5b37e1bba4888b9265d6f09f6e77b30b0881f067
Josué Juca <josuejuca19@gmail.com>
2022-10-05 20:46:57 -0300

    [ 05/10/2022 ] Atualização

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
-
 # cores ---------------------------------------
 co0 = "#FFFFFF"  # branca / white
 co1 = "#333333"  # preta pesado / dark black
@@ -19,10 +18,9 @@ fundo = "#3b3b3b" # preta / black
 # criando janela principal
 janela = Tk()
 janela.title('Jogo da Velha')
-janela.geometry('260x370')
 janela.iconbitmap('wolf-head.ico')
+janela.geometry('260x370')
 janela.configure(bg=fundo)
-
 
 
 # Dividindo a janela em 2 frames ---------------------------------------
@@ -35,9 +33,9 @@ frame_baixo.grid(row=1, column=0, sticky=NW)
 
 
 # Configurando o frame cima ---------------------------------------
-app_x = Label(frame_cima, text='F', height=1, relief='flat', anchor='center', font=('Ivy 40 bold'), bg=co1, fg=co7 )
+app_x = Label(frame_cima, text='X', height=1, relief='flat', anchor='center', font=('Ivy 40 bold'), bg=co1, fg=co7 )
 app_x.place(x=25, y=10)
-app_x = Label(frame_cima, text='Flamengo', height=1, relief='flat', anchor='center', font=('Ivy 7 bold'), bg=co1, fg=co0 )
+app_x = Label(frame_cima, text='Jogador 1', height=1, relief='flat', anchor='center', font=('Ivy 7 bold'), bg=co1, fg=co0 )
 app_x.place(x=17, y=70)
 app_x_pontos = Label(frame_cima, text='0', height=1, relief='flat', anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0 )
 app_x_pontos.place(x=80, y=20)
@@ -45,9 +43,9 @@ app_x_pontos.place(x=80, y=20)
 app_separador = Label(frame_cima, text=':', height=1, relief='flat', anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0 )
 app_separador.place(x=110, y=20)
 
-app_o = Label(frame_cima, text='P', height=1, relief='flat', anchor='center', font=('Ivy 40 bold'), bg=co1, fg=co4 )
+app_o = Label(frame_cima, text='O', height=1, relief='flat', anchor='center', font=('Ivy 40 bold'), bg=co1, fg=co4 )
 app_o.place(x=170, y=10)
-app_o = Label(frame_cima, text='Palmeiras', height=1, relief='flat', anchor='center', font=('Ivy 7 bold'), bg=co1, fg=co0 )
+app_o = Label(frame_cima, text='Jogador 2', height=1, relief='flat', anchor='center', font=('Ivy 7 bold'), bg=co1, fg=co0 )
 app_o.place(x=165, y=70)
 app_o_pontos = Label(frame_cima, text='0', height=1, relief='flat', anchor='center', font=('Ivy 30 bold'), bg=co1, fg=co0 )
 app_o_pontos.place(x=130, y=20)
@@ -391,12 +389,12 @@ def iniciar_jogo():
         
         if i =='X':
             score_2+=1
-            app_vencedor['text'] = 'Palmeiras'
+            app_vencedor['text'] = 'Joagador 2 venceu'
             app_o_pontos['text'] =score_2
             
         if i =='O':
             score_1+=1
-            app_vencedor['text'] = 'Flamengo'
+            app_vencedor['text'] = 'Joagador 1 venceu'
             app_x_pontos['text'] =score_1
         
         if i=='Foi empate':
